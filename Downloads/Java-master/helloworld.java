ef23b454add0e4a4b0f1e287beb0299dbfde84da
NKULULEKO NKOMO <22014247@live.mut.ac.za>
2023-06-15 10:13:59 +0200

    adding file

---
+++ Downloads/Java-master/helloworld.java
@@ -0,0 +1,8 @@
+public class helloworld{
+public static void main(String []args){
+
+System.out.println("hello world");
+System.out.println("hello world");
+System.out.printlin("hello world");
+}
+}
